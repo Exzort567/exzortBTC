fec4f1d42386042d9544cc08f047211ebe5c1e40
Kenneth <quibelkenneth@gmail.com>
2024-06-08 22:43:34 +0800

    feat: add hero section

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -28,97 +28,98 @@ export function FloatingNavDemo() {
     },
   ];
   return (
-    <div className="relative  w-full">
+    <div className="relative w-full z-10">
       <FloatingNav navItems={navItems} />
       <Navbar />
     </div>
   );
 }
+
 interface NavbarProps {
-    scrollToWebsiteDesign: () => void;
-    scrollToGraphicDesign: () => void;
-    scrollToShopifyStores: () => void;
-    scrollToBrands: () => void;
-    scrollToServices: () => void; // Define scrollToServices function
-  }
-  
-  const Navbar = ({
-    scrollToWebsiteDesign,
-    scrollToGraphicDesign,
-    scrollToShopifyStores,
-    scrollToBrands,
-    scrollToServices, // Add scrollToServices to props
-  }: NavbarProps) => {
-    const [isDropDownVisible, setIsDropDownVisible] = useState(false);
-  
-    const toggleDropDown = () => {
-      setIsDropDownVisible(!isDropDownVisible);
-    };
-  
-    const closeDropDown = () => {
-      setIsDropDownVisible(false);
-    };
-  
-    return (
-      <div>
-        <div className="p-6 md:p-10 flex items-center justify-between z-50">
-          <div>
-            <Link className="cursor-pointer" href="/">
-              <h2 className="text-slate-100">ExzortBTC</h2>
-            </Link>
-          </div>
-          <div
-            className="cursor-pointer hidden 
+  scrollToWebsiteDesign: () => void;
+  scrollToGraphicDesign: () => void;
+  scrollToShopifyStores: () => void;
+  scrollToBrands: () => void;
+  scrollToServices: () => void; // Define scrollToServices function
+}
+
+const Navbar = ({
+  scrollToWebsiteDesign,
+  scrollToGraphicDesign,
+  scrollToShopifyStores,
+  scrollToBrands,
+  scrollToServices, // Add scrollToServices to props
+}: NavbarProps) => {
+  const [isDropDownVisible, setIsDropDownVisible] = useState(false);
+
+  const toggleDropDown = () => {
+    setIsDropDownVisible(!isDropDownVisible);
+  };
+
+  const closeDropDown = () => {
+    setIsDropDownVisible(false);
+  };
+
+  return (
+    <div>
+      <div className="p-6 md:p-10 flex items-center justify-between z-50 bg-transparent">
+        <div>
+          <Link className="cursor-pointer" href="/">
+            <h2 className="text-slate-100">ExzortBTC</h2>
+          </Link>
+        </div>
+        <div
+          className="cursor-pointer hidden 
               md:flex space-x-10 items-center
                text-slate-300 text-center 
                bg-clip-text text-transparent 
                bg-gradient-to-b from-neutral-50
                 to bg-neutral-400 bg-opacity-50"
-          >
-            <div onClick={scrollToWebsiteDesign} className="hover:text-gray-50">
-              Website Design
-            </div>
-            <div onClick={scrollToGraphicDesign} className="hover:text-gray-50">
-              Graphic Design
-            </div>
-  
-            <div onClick={scrollToShopifyStores} className="hover:text-gray-50">
-              Shopify Stores
-            </div>
-            <div onClick={scrollToBrands} className="hover:text-gray-50">
-              Brands
-            </div>
-  
-            <Link href="/pricing" className="hover:text-gray-50">
-              Pricing
-            </Link>
+        >
+          <div onClick={scrollToWebsiteDesign} className="hover:text-gray-50">
+            Home
           </div>
-  
-          <div className="flex md:hidden">
-            {isDropDownVisible ? (
-              // display an x icon when the drop is visible
-              <div
-                onClick={toggleDropDown}
-                className="w-8 h-8 text-slate-300 cursor-pointer"
-              >
-                <X />
-                <DropDownMenu
-                  onClose={closeDropDown}
-                  scrollToServices={scrollToServices} // Pass scrollToServices
-                />
-              </div>
-            ) : (
-              <AlignJustify
-                onClick={toggleDropDown}
-                className="w-8 h-8 text-slate-300 cursor-pointer"
+          <div onClick={scrollToGraphicDesign} className="hover:text-gray-50">
+            About
+          </div>
+
+          <div onClick={scrollToShopifyStores} className="hover:text-gray-50">
+            Gallery
+          </div>
+          <div onClick={scrollToBrands} className="hover:text-gray-50">
+            Services
+          </div>
+
+          <Link href="/pricing" className="hover:text-gray-50">
+            Pricing
+          </Link>
+        </div>
+
+        <div className="flex md:hidden">
+          {isDropDownVisible ? (
+            // display an x icon when the drop is visible
+            <div
+              onClick={toggleDropDown}
+              className="w-8 h-8 text-slate-300 cursor-pointer"
+            >
+              <X />
+              <DropDownMenu
+                onClose={closeDropDown}
+                scrollToServices={scrollToServices} // Pass scrollToServices
               />
-            )}
-          </div>
-  
-          <div className="hidden md:flex">
-            <Link
-              href="/contact"
-              className="
+            </div>
+          ) : (
+            <AlignJustify
+              onClick={toggleDropDown}
+              className="w-8 h-8 text-slate-300 cursor-pointer"
+            />
+          )}
+        </div>
+
+        <div className="hidden md:flex">
+          <Link
+            href="/contact"
+            className="
               inline-flex h-12 animate-shimmer items-center justify-center 
               rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
               bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
@@ -126,12 +127,11 @@ interface NavbarProps {
                 focus:ring-offset-slate-50
   
               "
-            >
-              Contact
-            </Link>
-          </div>
+          >
+            Contact
+          </Link>
         </div>
       </div>
-      
-    );
-  };
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,66 @@
 import Image from "next/image";
 import { FloatingNavDemo } from "./navbar";
+import { Spotlight } from "@/components/ui/spotlight";
+import { FlipWords } from "../components/ui/flip-words";
+import { Meteors } from "@/components/ui/meteors";
 
 export default function Home() {
+  const words = ["Forex", "Crypto"];
   return (
-    <div className="w-full md:items-center bg-black/[0.86] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="w-full -screen md:items-center bg-black/[0.86] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <FloatingNavDemo/>
+      <Spotlight className="hidden md:flex md:left-80 md:-top-80" fill="white"/>
+
+      <div className="h-[40rem] flex flex-col justify-center items-center px-4">
+        <div className="text-4xl mx-auto font-normal text-neutral-400 dark:text-neutral-400 text-center">
+          <div className="flex justify-center">
+            <FlipWords words={words} />
+            <p className="text-white ml-2">Trader</p>
+          </div>
+          <p className="mt-4">
+            Expert Insights & Level-Headed<br />
+            Analysis on Crypto and Forex Markets
+          </p>
+        </div>
+        
+        <div className="mt-8 w-full relative max-w-xs">
+          <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-gray-300 to-gray-400 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+          <div className="relative shadow-xl bg-black/[0.87] border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+            <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-2 w-2 text-gray-300"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+                />
+              </svg>
+            </div>
+    
+            <h1 className="font-bold text-xl text-white mb-4 relative z-50">
+              Join our Telegram Channel
+            </h1>
+    
+            <p className="font-normal text-base text-slate-200 mb-4 relative z-50">
+            Join us to discover valuable insights, strategies, and support to excel in the exciting world of trading. 
+            </p>
+            
+            <a href="https://t.me/exzortBTC" target="_blank"> <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
+              Join here!
+            </button></a>
+           
+    
+            {/* Meaty part - Meteor effect */}
+            <Meteors number={20} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
