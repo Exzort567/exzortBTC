65d2fcd15ef3713d9948111b8b44952bf58c1d73
Kenneth <quibelkenneth@gmail.com>
2024-06-10 08:57:46 +0800

    fix: add optional chaining to safely invoke navItem.onClick in FloatingNav

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -61,7 +61,7 @@ export const FloatingNav = ({
             href={navItem.link}
             onClick={navItem.onClick ? (e) => {
               e.preventDefault();
-              navItem.onClick();
+              navItem.onClick?.();
             } : undefined}
             className={cn(
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
